4508ab8a8d533774fcac32910e5d23011020a311
Erika Scorsoni <scorsonie@gmail.com>
2020-05-14 17:20:09 -0300

    Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,9 @@
+console.log('a=', a)
+var a = 2
+console.log('a=', a)
+// hoisting seria o içamento. Não importa se definimos a variavel a depois, o programa reconhece.
+
+console.log('b=', b)
+let b = 2
+console.log('b=', b)
+// com let o içamento não funciona
